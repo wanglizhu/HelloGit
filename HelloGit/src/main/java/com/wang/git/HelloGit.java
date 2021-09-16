f424c8cfad7e4dc296329842f0e743813ab4e370
wanglizhu <280499781@qq.com>
2021-09-16 16:27:51 +0800

    创建01

---
+++ HelloGit/src/main/java/com/wang/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.wang.git;
+
+/**
+ * @author wlz
+ * @create 2021-09-16 16:26
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("01");
+    }
+}
